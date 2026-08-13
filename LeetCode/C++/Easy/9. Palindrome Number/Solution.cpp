682eeec080f6cc1815d7b57d8de66d60042920d5
Yashasvi-kaithal07 <yashasvi.25001302@kiet.edu>
2026-08-13 19:05:35 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/9. Palindrome Number/Solution.cpp
+++ LeetCode/C++/Easy/9. Palindrome Number/Solution.cpp
@@ -1,17 +1,37 @@
+// class Solution {
+// public:
+//     bool isPalindrome(int x) {
+
+//         if (x < 0) return false;
+
+//         int temp =x;
+//         long long rev=0;
+//         int ld;
+//     while(temp>0){
+//          ld= temp % 10;
+//         rev = rev * 10 +ld;
+//         temp = temp / 10;
+//         }
+//     return (rev == x); 
+//     }
+// };
+
 class Solution {
 public:
     bool isPalindrome(int x) {
+        // negative numbers palindrome nahi hote
+        // aur agar last digit 0 hai (except 0 itself), toh bhi palindrome nahi hoga
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
 
-        if (x < 0) return false;
-
-        int temp =x;
-        long long rev=0;
-        int ld;
-    while(temp>0){
-         ld= temp % 10;
-        rev = rev * 10 +ld;
-        temp = temp / 10;
+        int reversedHalf = 0;
+        while (x > reversedHalf) {
+            int ld = x % 10;
+            reversedHalf = reversedHalf * 10 + ld;
+            x /= 10;
         }
-    return (rev == x); 
+
+        // agar even digits hain toh x == reversedHalf
+        // agar odd digits hain toh reversedHalf/10 compare karna hoga
+        return (x == reversedHalf || x == reversedHalf / 10);
     }
 };
