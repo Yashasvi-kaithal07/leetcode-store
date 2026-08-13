ed35dc7c2def4f6e1e0db94888b614f363af10cf
Yashasvi-kaithal07 <yashasvi.25001302@kiet.edu>
2026-08-13 19:02:11 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/1. Two Sum/Solution.cpp
+++ LeetCode/C++/Easy/1. Two Sum/Solution.cpp
@@ -1,13 +1,29 @@
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         for (int i = 0; i < nums.size(); i++) {
+//             for (int j = i + 1; j < nums.size(); j++) {
+//                 if (nums[i] + nums[j] == target) {
+//                     return {i, j};  // agar sum target ke equal hai toh indices return karo
+//                 }
+//             }
+//         }
+//         return {}; // agar koi pair nahi mila toh empty vector return karo
+//     }
+// };
+
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp; // value -> index
         for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                if (nums[i] + nums[j] == target) {
-                    return {i, j};  // agar sum target ke equal hai toh indices return karo
-                }
+            int complement = target - nums[i];
+            if (mp.find(complement) != mp.end()) {
+                return {mp[complement], i};
             }
+            mp[nums[i]] = i;
         }
-        return {}; // agar koi pair nahi mila toh empty vector return karo
+        return {};
     }
 };
