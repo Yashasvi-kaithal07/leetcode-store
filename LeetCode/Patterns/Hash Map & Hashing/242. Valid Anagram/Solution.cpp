7b361b5243c8dbf54b7227b2328541c81638cf27
Yashasvi-kaithal07 <yashasvi.25001302@kiet.edu>
2026-08-18 23:27:52 +0530

    Add C++ Solution to Patterns/Hash Map & Hashing

--- LeetCode/Patterns/Hash Map & Hashing/242. Valid Anagram/Solution.cpp
+++ LeetCode/Patterns/Hash Map & Hashing/242. Valid Anagram/Solution.cpp
@@ -1,32 +1,52 @@
 
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+
+//           if(s.size() != t.size())
+//         {
+//             return false;
+//         }
+
+//         // Step 2: Character frequency
+//         int freq[26] = {0};
+
+//         for(int i = 0; i < s.size(); i++)
+//         {
+//             freq[s[i] - 'a']++;
+//             freq[t[i] - 'a']--;
+//         }
+
+//         // Step 3: Check frequency
+//         for(int i = 0; i < 26; i++)
+//         {
+//             if(freq[i] != 0)
+//             {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
+
+    #include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
 
-          if(s.size() != t.size())
-        {
-            return false;
+        unordered_map<char,int> freq;
+
+        for(int i = 0; i < s.size(); i++) {
+            freq[s[i]]++;
+            freq[t[i]]--;
         }
 
-        // Step 2: Character frequency
-        int freq[26] = {0};
-
-        for(int i = 0; i < s.size(); i++)
-        {
-            freq[s[i] - 'a']++;
-            freq[t[i] - 'a']--;
-        }
-
-        // Step 3: Check frequency
-        for(int i = 0; i < 26; i++)
-        {
-            if(freq[i] != 0)
-            {
-                return false;
-            }
+        for(auto &p : freq) {
+            if(p.second != 0) return false;
         }
         return true;
     }
 };
-
-    
