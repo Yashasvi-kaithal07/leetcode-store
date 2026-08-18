15d608dd3479130a9dec387a4baac05914454a8b
Yashasvi-kaithal07 <yashasvi.25001302@kiet.edu>
2026-08-19 00:19:09 +0530

    Add Easy C++ Solution from LeetCode

---
+++ LeetCode/C++/Easy/387. First Unique Character in a String/Solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        // Step 1: Frequency count
+      int f[26]={0};
+      for(int i=0; i < s.size(); i++){
+        f[s[i]-'a']++;
+      }
+
+        // Step 2: Find first unique
+        for(int i = 0; i < s.size(); i++) {
+            if(f[s[i] - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+ 
