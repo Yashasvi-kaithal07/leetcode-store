eb1464e839bf5e5c9717c4b122cdce2560ebd0e7
Yashasvi-kaithal07 <yashasvi.25001302@kiet.edu>
2026-08-30 14:41:39 +0530

    Add Medium C++ Solution for 53. Maximum Subarray from LeetCode

--- LeetCode/Patterns/Dynamic Programming/53. Maximum Subarray/Solution.cpp
+++ LeetCode/Patterns/Dynamic Programming/53. Maximum Subarray/Solution.cpp
@@ -2,17 +2,34 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int n = nums.size();
-        int maxsum = INT_MIN; 
+         int maxsum = INT_MIN; 
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                int sum = 0;
-                for (int k = i; k <= j; k++) {
-                    sum += nums[k];
-                }
-                maxsum = max(maxsum, sum); 
-            }
-        }
-        return maxsum; 
+        //BRUTE FORCE
+    //     for (int i = 0; i < n; i++) {
+    //         for (int j = i; j < n; j++) {
+    //             int sum = 0;
+    //             for (int k = i; k <= j; k++) {
+    //                 sum += nums[k];
+    //             }
+    //             maxsum = max(maxsum, sum); 
+    //         }
+    //     }
+    //     return maxsum; 
+    // }
+
+
+    for(int i = 0; i < n; i++)
+{
+    int sum = 0;
+
+    for(int j = i; j < n; j++)
+    {
+        sum += nums[j];
+
+        maxsum = max(maxsum, sum);
     }
+}
+return maxsum;
+}
+
 };
