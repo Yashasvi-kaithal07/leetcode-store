95ebad718a92a33b2d9283723dd2f24543376233
Yashasvi-kaithal07 <yashasvi.25001302@kiet.edu>
2026-09-01 02:39:36 +0530

    Add Easy C++ Solution for 219. Contains Duplicate II from LeetCode

--- LeetCode/C++/Easy/219. Contains Duplicate II/Solution.cpp
+++ LeetCode/C++/Easy/219. Contains Duplicate II/Solution.cpp
@@ -15,5 +15,23 @@ public:
 //    return false;
 
 
+        unordered_map<int, int> mp;
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            // Agar element pehle se map mein hai
+            if(mp.find(nums[i]) != mp.end())
+            {
+                // Current index - previous index
+                if(i - mp[nums[i]] <= k)
+                {
+                    return true;
+                }
+            }
+
+            mp[nums[i]] = i;
+        }
+
+        return false;
     }
 };
