75bfd550f197f5673bd050e9a9279b4c50b0b4e2
Yashasvi-kaithal07 <yashasvi.25001302@kiet.edu>
2026-09-10 01:00:31 +0530

    Add Easy C++ Solution for 1603. Running Sum of 1d Array from LeetCode

--- LeetCode/C++/Easy/1603. Running Sum of 1d Array/Solution.cpp
+++ LeetCode/C++/Easy/1603. Running Sum of 1d Array/Solution.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        vector<int>ans;
-        for (int i = 0; i < nums.size(); i++) {
-            int sum = 0;
-
-            for (int j = 0; j <= i; j++) {
-                sum += nums[j];
-            }
-
-            ans.push_back(sum);
+       
+        for (int i = 1; i < nums.size(); i++) {
+            nums[i] = nums[i] + nums[i - 1];
         }
 
-        return ans;
+        return nums;
     }
 };
--- LeetCode/Patterns/Prefix Sum/1603. Running Sum of 1d Array/README.md
+++ LeetCode/Patterns/Prefix Sum/1603. Running Sum of 1d Array/README.md
@@ -8,8 +8,8 @@
 Array, Prefix Sum
 
 ### 🚀 Performance
-- **Runtime:** 4 ms
-- **Memory:** 12.6 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
