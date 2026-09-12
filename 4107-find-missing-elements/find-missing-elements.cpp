895ab8e9a3fdc07b72567fa149e79b75c160baf3
Yashasvi-kaithal07 <yashasvi.25001302@kiet.edu>
2026-09-12 22:42:01 +0530

    Time: 1 ms (63.19%) | Memory: 32.3 MB (77.43%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
-        int mn = *min_element(nums.begin(), nums.end());
-        int mx = *max_element(nums.begin(), nums.end());
+        // int mn = *min_element(nums.begin(), nums.end());
+        // int mx = *max_element(nums.begin(), nums.end());
 
-        unordered_set<int> st(nums.begin(), nums.end());
+        // unordered_set<int> st(nums.begin(), nums.end());
+
+        // vector<int> ans;
+
+        // for(int num = mn; num <= mx; num++) {
+        //     if(st.find(num) == st.end()) {
+        //         ans.push_back(num);
+        //     }
+        // }
+
+        // return ans;
+
+           int mn = *min_element(nums.begin(), nums.end());
+        int mx = *max_element(nums.begin(), nums.end());
 
         vector<int> ans;
 
         for(int num = mn; num <= mx; num++) {
-            if(st.find(num) == st.end()) {
+            
+            // STL find()
+            if(find(nums.begin(), nums.end(), num) == nums.end()) {
                 ans.push_back(num);
             }
         }
 
         return ans;
+				
     }
 };
