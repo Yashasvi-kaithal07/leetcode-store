29fef4fe68eed214d2d827e9948bdc8f7c3efda6
Yashasvi-kaithal07 <yashasvi.25001302@kiet.edu>
2026-09-14 23:07:06 +0530

    Time: 20 ms (37.07%) | Memory: 26.1 MB (43.75%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -3,30 +3,50 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
      unordered_map<string, vector<string>> mp;
 
-        for(string s : strs) {
+        // for(string s : strs) {
             
-            vector<int> freq(26, 0);
+        //     vector<int> freq(26, 0);
 
-            for(char ch : s) {
-                freq[ch - 'a']++;
-            }
+        //     for(char ch : s) {
+        //         freq[ch - 'a']++;
+        //     }
 
-            // Convert frequency array into a string key
-            string key = "";
+        //     // Convert frequency array into a string key
+        //     string key = "";
 
-            for(int count : freq) {
-                key += to_string(count) + "#";
-            }
+        //     for(int count : freq) {
+        //         key += to_string(count) + "#";
+        //     }
+
+        //     mp[key].push_back(s);
+        // }
+
+        // vector<vector<string>> ans;
+
+        // for(auto it : mp) {
+        //     ans.push_back(it.second);
+        // }
+
+        // return ans;
+   
+
+
+
+        for (string s : strs) {
+
+            string key = s;
+            sort(key.begin(), key.end());
 
             mp[key].push_back(s);
         }
 
         vector<vector<string>> ans;
 
-        for(auto it : mp) {
+        for (auto it : mp) {
             ans.push_back(it.second);
         }
 
         return ans;
     }
 };
+   
