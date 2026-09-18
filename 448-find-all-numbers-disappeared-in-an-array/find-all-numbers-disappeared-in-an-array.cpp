c59a25472a4e0b07599c530503ab65760a28aa5c
Yashasvi-kaithal07 <yashasvi.25001302@kiet.edu>
2026-09-18 19:33:51 +0530

    Time: 4 ms (66.34%) | Memory: 53.1 MB (76.42%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
@@ -1,18 +1,7 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        // int n = nums.size();
         
-        // unordered_set<int> st(nums.begin(), nums.end());
-        // vector<int> ans;
-
-        // for(int i = 1; i <= n; i++) {
-        //     if(st.find(i) == st.end()) {
-        //         ans.push_back(i);
-        //     }
-        // }
-
-        // return ans;
 
             int n = nums.size();
         vector<int> ans;
